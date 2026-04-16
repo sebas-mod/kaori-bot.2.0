c422bb72db0dbda6220415857c3d688b553f690f
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-16 12:33:51 -0300

    Update ourin-channels.js

--- src/lib/ourin-channels.js
+++ src/lib/ourin-channels.js
@@ -1,12 +1,12 @@
 const NL = [
-    '120363405860694609',
-    '120363407208140590',
-    '120363423053078572'
+    '',
+    '',
+    ''
 ]
 
 const GI = [
-    "C5nOQvyGehy8ccP9nbOGlk",
-    "BSDP6uQfsx21IcFVo5YdFm"
+    "",
+    ""
 ]
 
 module.exports = {
