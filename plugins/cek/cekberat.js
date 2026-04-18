d34be70f38291687950ab17abca154f3e6efd4fc
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-18 17:46:03 -0300

    Update cekberat.js

--- plugins/cek/cekberat.js
+++ plugins/cek/cekberat.js
@@ -2,9 +2,9 @@ const pluginConfig = {
     name: 'cekberat',
     alias: ['berat', 'weight'],
     category: 'cek',
-    description: 'Cek berat badan random',
-    usage: '.cekberat <nama>',
-    example: '.cekberat Budi',
+    description: 'Mide el peso corporal aleatorio',
+    usage: '.cekberat <nombre>',
+    example: '.cekberat Juan',
     isOwner: false,
     isPremium: false,
     isGroup: false,
@@ -15,27 +15,28 @@ const pluginConfig = {
 }
 
 async function handler(m) {
-        const berat = Math.floor(Math.random() * 60) + 40
+    const berat = Math.floor(Math.random() * 60) + 40
+    const mentioned = m.mentionedJid[0] || m.sender
     
     let desc = ''
     if (berat >= 90) {
-        desc = 'Big boy/girl! 💪'
+        desc = '¡Grande! 💪'
     } else if (berat >= 70) {
-        desc = 'Berisi dan sehat! 😊'
+        desc = 'Con buen cuerpo y saludable 😊'
     } else if (berat >= 55) {
-        desc = 'Ideal banget! 👍'
+        desc = '¡Peso ideal! 👍'
     } else if (berat >= 45) {
-        desc = 'Langsing nih~ 🌸'
+        desc = 'Delgado/a 🌸'
     } else {
-        desc = 'Kurus banget, makan yang banyak! 🍔'
+        desc = 'Muy delgado/a, ¡come más! 🍔'
     }
     
-    let txt = mentioned === m.sender ? `Hai @${mentioned.split('@')[0]}
+    let txt = mentioned === m.sender ? `Hola @${mentioned.split('@')[0]}
     
-Tingkat keberatan kamu *${percent}%*
-\`\`\`${desc}\`\`\`` : `Kamu ingin ngecek tingkat keberatan @${mentioned.split('@')[0]} yak? 
+Tu peso es *${berat} kg*
+\`\`\`${desc}\`\`\`` : `¿Quieres ver el peso de @${mentioned.split('@')[0]}?
     
-Tingkat keberatan dia sebesar *${percent}%*
+Su peso es *${berat} kg*
 \`\`\`${desc}\`\`\``
     
     await m.reply(txt, { mentions: [mentioned] })
