83f26e5743890bc51e72c0f2a36cde5121ed29de
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-19 01:42:24 -0300

    Update cekbucin.js

--- plugins/cek/cekbucin.js
+++ plugins/cek/cekbucin.js
@@ -1,44 +1,46 @@
 const pluginConfig = {
     name: 'cekbucin',
     alias: ['bucin'],
-    category: 'cek',
-    description: 'Cek seberapa bucin kamu',
-    usage: '.cekbucin <nama>',
+    category: 'chequeo',
+    description: 'Verifica qué tan enamorado obsesivo eres',
+    usage: '.cekbucin <nombre>',
     example: '.cekbucin Budi',
     isOwner: false,
     isPremium: false,
     isGroup: false,
     isPrivate: false,
     cooldown: 5,
-    energi: 0,
+    energia: 0,
     isEnabled: true
 }
 
 async function handler(m) {
-        const percent = Math.floor(Math.random() * 101)
+    const percent = Math.floor(Math.random() * 101)
     const mentioned = m.mentionedJid[0] || m.sender
                     
     let desc = ''
     if (percent >= 90) {
-        desc = 'BUCIN AKUT! Udah gabisa diselamatkan 😭💔'
+        desc = '¡ENAMORADO OBSESIVO EXTREMO! Ya no tiene salvación 😭💔'
     } else if (percent >= 70) {
-        desc = 'Bucin parah nih~ 🥺'
+        desc = 'Muy enamorado obsesivo 🥺'
     } else if (percent >= 50) {
-        desc = 'Lumayan bucin 💕'
+        desc = 'Bastante enamorado 💕'
     } else if (percent >= 30) {
-        desc = 'Sedikit bucin 😊'
+        desc = 'Un poco enamorado 😊'
     } else {
-        desc = 'Santai aja, gak bucin 😎'
+        desc = 'Tranquilo, no estás obsesionado 😎'
     }
     
-    let txt = mentioned === m.sender ? `Hai @${mentioned.split('@')[0]}
+    let txt = mentioned === m.sender 
+    ? `Hola @${mentioned.split('@')[0]}
     
-Tingkat kebucinan kamu *${percent}%*
-\`\`\`${desc}\`\`\`` : `Kamu ingin ngecek tingkat kebucinan @${mentioned.split('@')[0]} yak? 
+Tu nivel de obsesión amorosa es *${percent}%*
+\`\`\`${desc}\`\`\`` 
+    : `¿Quieres comprobar el nivel de obsesión amorosa de @${mentioned.split('@')[0]}?
     
-Tingkat kebucinan dia sebesar *${percent}%*
+Su nivel es *${percent}%*
 \`\`\`${desc}\`\`\``
-    
+
     await m.reply(txt, { mentions: [mentioned] })
 }
 
