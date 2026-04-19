c153d2f60e7e8c1786e6926f921fbcfe71988362
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-19 01:47:54 -0300

    Update gacha plugin for language and terminology

--- plugins/cek/cekgacha.js
+++ plugins/cek/cekgacha.js
@@ -1,36 +1,36 @@
 const pluginConfig = {
     name: 'cekgacha',
-    alias: ['gacha', 'luck'],
-    category: 'cek',
-    description: 'Cek hoki gacha kamu',
-    usage: '.cekgacha <nama>',
+    alias: ['gacha', 'suerte'],
+    category: 'chequeo',
+    description: 'Verifica tu suerte en gacha',
+    usage: '.cekgacha <nombre>',
     example: '.cekgacha Budi',
     isOwner: false,
     isPremium: false,
     isGroup: false,
     isPrivate: false,
     cooldown: 5,
-    energi: 0,
+    energia: 0,
     isEnabled: true
 }
 
 async function handler(m) {
-        const percent = Math.floor(Math.random() * 101)
+    const percent = Math.floor(Math.random() * 101)
     const mentioned = m.mentionedJid[0] || m.sender
                     
     let desc = ''
-    if (percent >= 90) desc = 'HOKI PARAH! SSR GUARANTEED! ✨💎'
-    else if (percent >= 70) desc = 'Lucky! Pasti dapet SR keatas! 🍀'
-    else if (percent >= 50) desc = 'Hoki-hoki dikit 😊'
-    else if (percent >= 30) desc = 'Hmm... pray harder! 🙏'
-    else desc = 'SIAL! Nanti aja gachanya! 💔'
+    if (percent >= 90) desc = '¡SUERTE EXTREMA! ¡SSR GARANTIZADO! ✨💎'
+    else if (percent >= 70) desc = '¡Suerte! Seguro consigues SR o más 🍀'
+    else if (percent >= 50) desc = 'Algo de suerte 😊'
+    else if (percent >= 30) desc = 'Hmm... reza más 🙏'
+    else desc = '¡MALA SUERTE! Mejor intenta después 💔'
     
-    let txt = mentioned === m.sender ? `Hai @${mentioned.split('@')[0]}
+    let txt = mentioned === m.sender ? `Hola @${mentioned.split('@')[0]}
     
-Tingkat kegachaan kamu *${percent}%*
-\`\`\`${desc}\`\`\`` : `Kamu ingin ngecek tingkat kegachaan @${mentioned.split('@')[0]} yak? 
+Tu nivel de suerte en gacha es *${percent}%*
+\`\`\`${desc}\`\`\`` : `¿Quieres comprobar el nivel de suerte en gacha de @${mentioned.split('@')[0]}?
     
-Tingkat kegachaan dia sebesar *${percent}%*
+Su nivel es *${percent}%*
 \`\`\`${desc}\`\`\``
     
     await m.reply(txt, { mentions: [mentioned] })
